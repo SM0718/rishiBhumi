2d39490a029ada58bb7a204add3e8077feb8fbb0
Sagnik Majumder <150660189+SM0718@users.noreply.github.com>
2024-01-25 17:19:09 +0530

    Update SelectExclusivePuja.jsx

--- src/pages/SelectExclusivePuja.jsx
+++ src/pages/SelectExclusivePuja.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useParams, useNavigate } from 'react-router-dom'
+import { Link, useParams, useNavigate } from 'react-router-dom'
 import { exclusivePooja } from '../infos/exclusivePooja'
 import Button from '../components/Button';
 
