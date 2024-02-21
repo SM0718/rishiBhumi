6113eb573a57e501d70f03e652f38ef6b18b93a9
Sagnik Majumder <150660189+SM0718@users.noreply.github.com>
2024-02-21 12:17:39 +0530

    Update cartItems.js

--- src/infos/cartItems.js
+++ src/infos/cartItems.js
@@ -3,430 +3,499 @@ export const cartItems = [
         name: "Satyanarayan Pooja",
         pic: "./WeeklyPooja/1.jpg",
         price: 501,
-        slug: "satyanarayan-pooja"
+        slug: "satyanarayan-pooja",
+        paymentLink: "https://buy.stripe.com/test_cN28y46Mc5285nqcMM"
     },
     {
         name: "Shani Graha Shanti Pooja",
         pic: "./WeeklyPooja/2.jpg",
         price: 501,
-        slug: "shani-graha-shanti-pooja"
+        slug: "shani-graha-shanti-pooja",
+        paymentLink: "https://buy.stripe.com/test_cN28y46Mc5285nqcMM"
     },
     {
         name: "Ganesh Pooja",
         pic: "./WeeklyPooja/3.webp",
         price: 501,
-        slug: "ganesh-pooja"
+        slug: "ganesh-pooja",
+        paymentLink: "https://buy.stripe.com/test_cN28y46Mc5285nqcMM"
     },
     {
         name: "Shiv Pooja",
         pic: "./WeeklyPooja/4.jpg",
         price: 501,
-        slug: "shiv-pooja"
+        slug: "shiv-pooja",
+        paymentLink: "https://buy.stripe.com/test_cN28y46Mc5285nqcMM"
     },
     {
         name: "Baba Lokenath Pooja",
         pic: "./WeeklyPooja/5.jpg",
         price: 501,
-        slug: "baba-lokenath-pooja"
+        slug: "baba-lokenath-pooja",
+        paymentLink: "https://buy.stripe.com/test_cN28y46Mc5285nqcMM"
     },
     {
         name: "Shri Laxmi Pooja",
         pic: "./WeeklyPooja/6.jpg",
         price: 501,
-        slug: "shri-laxmi-pooja"
+        slug: "shri-laxmi-pooja",
+        paymentLink: "https://buy.stripe.com/test_cN28y46Mc5285nqcMM"
     },
     {
         name: "Radha Krishna Pooja",
         pic: "./WeeklyPooja/7.jpg",
         price: 501,
-        slug: "radha-krishna-pooja"
+        slug: "radha-krishna-pooja",
+        paymentLink: "https://buy.stripe.com/test_cN28y46Mc5285nqcMM"
     },
     {
         name: "Gau Mata Pooja",
         pic: "./WeeklyPooja/8.jpg",
         price: 501,
-        slug: "gau-mata-pooja"
+        slug: "gau-mata-pooja",
+        paymentLink: "https://buy.stripe.com/test_cN28y46Mc5285nqcMM"
     },
     {
         name: "Maha Rudrabhishek Pooja",
         pic: "./WeeklyPooja/9.jpg",
         price: 501,
-        slug: "maha-rudrabhishek-pooja"
+        slug: "maha-rudrabhishek-pooja",
+        paymentLink: "https://buy.stripe.com/test_cN28y46Mc5285nqcMM"
     },
     {
         name: "Hanuman Pooja",
         pic: "./WeeklyPooja/10.jpg",
         price: 501,
-        slug: "hanuman-pooja"
+        slug: "hanuman-pooja",
+        paymentLink: "https://buy.stripe.com/test_cN28y46Mc5285nqcMM"
     },
     {
         name: "Mahalaxmi Aarti",
         pic: "./WeeklyPooja/11.jpg",
         price: 501,
-        slug: "mahalaxmi-aarti"
+        slug: "mahalaxmi-aarti",
+        paymentLink: "https://buy.stripe.com/test_cN28y46Mc5285nqcMM"
     },
     {
         name: "Maa Mangal Chandi Pooja",
         pic: "./WeeklyPooja/12.jpg",
         price: 501,
-        slug: "maa-mangal-chandi-pooja"
+        slug: "maa-mangal-chandi-pooja",
+        paymentLink: "https://buy.stripe.com/test_cN28y46Mc5285nqcMM"
     },
     {
         name: "Sawaraswati Pooja",
         pic: "./WeeklyPooja/13.jpg",
         price: 501,
-        slug: "sawaraswati-pooja"
+        slug: "sawaraswati-pooja",
+        paymentLink: "https://buy.stripe.com/test_cN28y46Mc5285nqcMM"
     },
     {
         name: "Navgraha Shanti Pooja",
         pic: "./WeeklyPooja/14.jpg",
         price: 501,
-        slug: "navgraha-shanti-pooja"
+        slug: "navgraha-shanti-pooja",
+        paymentLink: "https://buy.stripe.com/test_cN28y46Mc5285nqcMM"
     },
     {
         name: "Kali Puja during Amavasya",
         pic: "./WeeklyPooja/15.jpg",
         price: 501,
-        slug: "kali-pooja-amavasya"
+        slug: "kali-pooja-amavasya",
+        paymentLink: "https://buy.stripe.com/test_cN28y46Mc5285nqcMM"
     },
     {
         name: "Rahu Pooja",
         pic: "./SpecialPooja/1.webp",
         price: 10999,
-        slug: "rahu-puja"
+        slug: "rahu-puja",
+        paymentLink: "https://buy.stripe.com/test_8wMdSo7QgfGM2be6os"
     },
     {
         name: "Ketu Grah Pooja",
         pic: "./SpecialPooja/2.webp",
         price: 10999,
-        slug: "ketu-grah-pooja"
+        slug: "ketu-grah-pooja",
+        paymentLink: "https://buy.stripe.com/test_8wMdSo7QgfGM2be6os"
     },
     {
         name: "Kaal Sarp Dosha Pooja",
         pic: "./SpecialPooja/3.jpg",
         price: 10999,
-        slug: "kaal-sarp-dosha-pooja"
+        slug: "kaal-sarp-dosha-pooja",
+        paymentLink: "https://buy.stripe.com/test_8wMdSo7QgfGM2be6os"
     },
     {
         name: "Grahan Dosh Pooja",
         pic: "./SpecialPooja/4.webp",
         price: 10999,
-        slug: "grahan-dosh-pooja"
+        slug: "grahan-dosh-pooja",
+        paymentLink: "https://buy.stripe.com/test_8wMdSo7QgfGM2be6os"
     },
     {
         name: "Guru Chandal Dosh Pooja",
         pic: "./SpecialPooja/5.jpg",
         price: 10999,
-        slug: "guru-chandal-dosh-pooja"
+        slug: "guru-chandal-dosh-pooja",
+        paymentLink: "https://buy.stripe.com/test_8wMdSo7QgfGM2be6os"
     },
     {
         name: "Shani-Ketu Shrapit Dosha Pooja",
         pic: "./SpecialPooja/6.webp",
         price: 10999,
-        slug: "shanti-ketu-pooja"
+        slug: "shanti-ketu-pooja",
+        paymentLink: "https://buy.stripe.com/test_8wMdSo7QgfGM2be6os"
     },
     {
         name: "Mangal Graha Shanti Pooja",
         pic: "./SpecialPooja/7.jpg",
         price: 10999,
-        slug: "mangal-graha-shanti-pooja"
+        slug: "mangal-graha-shanti-pooja",
+        paymentLink: "https://buy.stripe.com/test_8wMdSo7QgfGM2be6os"
     },
     {
         name: "Chandra Grah Shanti Pooja",
         pic: "./SpecialPooja/8.png",
         price: 10999,
-        slug: "chandra-grah-shanti-pooja"
+        slug: "chandra-grah-shanti-pooja",
+        paymentLink: "https://buy.stripe.com/test_8wMdSo7QgfGM2be6os"
     },
     {
         name: "Vivah Badha Nibaran Pooja",
         pic: "./SpecialPooja/9.jpg",
         price: 10999,
-        slug: "vivah-bandha-nibaran-pooja"
+        slug: "vivah-bandha-nibaran-pooja",
+        paymentLink: "https://buy.stripe.com/test_8wMdSo7QgfGM2be6os"
     },
     {
         name: "Pitra Dosh Nibaran Pooja",
         pic: "./SpecialPooja/10.jpg",
         price: 10999,
-        slug: "pitra-dosh-nibaran-pooja"
+        slug: "pitra-dosh-nibaran-pooja",
+        paymentLink: "https://buy.stripe.com/test_8wMdSo7QgfGM2be6os"
     },
     {
         name: "Buri Nazar Removal Pooja",
         pic: "./SpecialPooja/11.jpg",
         price: 10999,
-        slug: "buri-nazar-removal-pooja"
+        slug: "buri-nazar-removal-pooja",
+        paymentLink: "https://buy.stripe.com/test_8wMdSo7QgfGM2be6os"
     },
     {
         name: "Navgraha Shanti Pooja",
         pic: "./SpecialPooja/12.jpg",
         price: 10999,
-        slug: "navgraha-shanti-pooja"
+        slug: "navgraha-shanti-pooja",
+        paymentLink: "https://buy.stripe.com/test_8wMdSo7QgfGM2be6os"
     },
     {
         name: "Nazar Raksha Kawach",
         pic: "./Kawach/1.jpg",
-        price: 10001,
-        slug: "nazar-raksha-kawach"
+        price: 10999,
+        slug: "nazar-raksha-kawach",
+        paymentLink: "https://buy.stripe.com/test_aEU5lS8Uk528dTW5kl"
     },
     {
         name: "Tara Kawach",
         pic: "./Kawach/2.png",
-        price: 10001,
-        slug: "tara-kawach"
+        price: 10999,
+        slug: "tara-kawach",
+        paymentLink: "https://buy.stripe.com/test_aEU5lS8Uk528dTW5kl"
     },
     {
         name: "Rahu Kawach",
         pic: "./Kawach/3.jpg",
-        price: 10001,
-        slug: "rahu-kawach"
+        price: 10999,
+        slug: "rahu-kawach",
+        paymentLink: "https://buy.stripe.com/test_aEU5lS8Uk528dTW5kl"
     },
     {
         name: "Surya Kawach",
         pic: "./Kawach/4.webp",
-        price: 10001,
-        slug: "surya-kawach"
+        price: 10999,
+        slug: "surya-kawach",
+        paymentLink: "https://buy.stripe.com/test_aEU5lS8Uk528dTW5kl"
     },
     {
         name: "Shani Grah Shanti Kawach",
         pic: "./Kawach/5.jpg",
-        price: 10001,
-        slug: "shani-grah-shanti-kawach"
+        price: 10999,
+        slug: "shani-grah-shanti-kawach",
+        paymentLink: "https://buy.stripe.com/test_aEU5lS8Uk528dTW5kl"
     },
     {
         name: "Mahamrityunjay Kawach",
         pic: "./Kawach/6.png",
-        price: 10001,
-        slug: "mahamrityunjay-kawach"
+        price: 10999,
+        slug: "mahamrityunjay-kawach",
+        paymentLink: "https://buy.stripe.com/test_aEU5lS8Uk528dTW5kl"
     },
     {
         name: "Ketu Kawach",
         pic: "./Kawach/7.jpg",
-        price: 10001,
-        slug: "ketu-kawach"
+        price: 10999,
+        slug: "ketu-kawach",
+        paymentLink: "https://buy.stripe.com/test_aEU5lS8Uk528dTW5kl"
     },
     {
         name: "Mangal Kawach",
         pic: "./Kawach/8.jpg",
-        price: 10001,
-        slug: "mangal-kawach"
+        price: 10999,
+        slug: "mangal-kawach",
+        paymentLink: "https://buy.stripe.com/test_aEU5lS8Uk528dTW5kl"
     },
     {
         name: "Shukra Grah Kawach",
         pic: "./Kawach/9.jpg",
-        price: 10001,
-        slug: "shukra-grah-kawach"
+        price: 10999,
+        slug: "shukra-grah-kawach",
+        paymentLink: "https://buy.stripe.com/test_aEU5lS8Uk528dTW5kl"
     },
     {
         name: "Chandra Kawach",
         pic: "./Kawach/10.jpg",
-        price: 10001,
-        slug: "chandra-kawach"
+        price: 10999,
+        slug: "chandra-kawach",
+        paymentLink: "https://buy.stripe.com/test_aEU5lS8Uk528dTW5kl"
     },
     {
         name: "Pitr Dosh Nivaran Kawach",
         pic: "./Kawach/11.webp",
-        price: 10001,
-        slug: "pitr-dosh-nivaran-kawach"
+        price: 10999,
+        slug: "pitr-dosh-nivaran-kawach",
+        paymentLink: "https://buy.stripe.com/test_aEU5lS8Uk528dTW5kl"
     },
     {
         name: "Apar Dhan Prapti Kawach",
         pic: "./Kawach/12.jpeg",
-        price: 10001,
-        slug: "apar-dhan-prapti-kawach"
+        price: 10999,
+        slug: "apar-dhan-prapti-kawach",
+        paymentLink: "https://buy.stripe.com/test_aEU5lS8Uk528dTW5kl"
     },
     {
         name: "Nav Grah Shanti Kavach",
         pic: "./Kawach/13.jpg",
-        price: 10001,
-        slug: "nav-grah-shanti-kawach"
+        price: 10999,
+        slug: "nav-grah-shanti-kawach",
+        paymentLink: "https://buy.stripe.com/test_aEU5lS8Uk528dTW5kl"
     },
     {
         name: "Ganesh kawach",
         pic: "./Kawach/14.webp",
-        price: 10001,
-        slug: "ganesh-kawach"
+        price: 10999,
+        slug: "ganesh-kawach",
+        paymentLink: "https://buy.stripe.com/test_aEU5lS8Uk528dTW5kl"
     },
     {
         name: "Raj Rajeswari Kawach",
         pic: "./Kawach/15.jpg",
-        price: 10001,
-        slug: "raj-rajeswari-kawach"
+        price: 10999,
+        slug: "raj-rajeswari-kawach",
+        paymentLink: "https://buy.stripe.com/test_aEU5lS8Uk528dTW5kl"
     },
     {
         name: "Neel Saraswati Kawach",
         pic: "./Kawach/16.jpg",
-        price: 10001,
-        slug: "neel-saraswati-kawach"
+        price: 10999,
+        slug: "neel-saraswati-kawach",
+        paymentLink: "https://buy.stripe.com/test_aEU5lS8Uk528dTW5kl"
     },
     {
         name: "Brahaspati Kawach",
         pic: "./Kawach/17.jpg",
-        price: 10001,
-        slug: "brahaspati-kawach"
+        price: 10999,
+        slug: "brahaspati-kawach",
+        paymentLink: "https://buy.stripe.com/test_aEU5lS8Uk528dTW5kl"
     },
     {
         name: "Budh Grah Kawach",
         pic: "./Kawach/18.jpg",
-        price: 10001,
-        slug: "budh-grah-kawach"
+        price: 10999,
+        slug: "budh-grah-kawach",
+        paymentLink: "https://buy.stripe.com/test_aEU5lS8Uk528dTW5kl"
     },
     {
         name: "Kaal Sarp Yog Nashak Kawach",
         pic: "./Kawach/19.jpg",
-        price: 10001,
-        slug: "kaal-sarp-kawach"
+        price: 10999,
+        slug: "kaal-sarp-kawach",
+        paymentLink: "https://buy.stripe.com/test_aEU5lS8Uk528dTW5kl"
     },
     {
         name: "Devi Soroshi Kawach",
         pic: "./Kawach/20.jpeg",
-        price: 10001,
-        slug: "devi-soroshi-kawach"
+        price: 10999,
+        slug: "devi-soroshi-kawach",
+        paymentLink: "https://buy.stripe.com/test_aEU5lS8Uk528dTW5kl"
     },
     {
         name: "Pyrite Bracelet",
         crystalPic: "./Crystals/1.webp",
         price: 999,
-        slug: "pyrite-bracelet"
+        slug: "pyrite-bracelet",
+        paymentLink: "https://buy.stripe.com/test_5kA4hOgmMdyE4jmeUW"
     },
     {
         name: "Green Gventurine Bracelet",
         pic: "./Crystals/2.webp",
-        price: 799,
-        slug: "gventurine-bracelet"
+        price: 999,
+        slug: "gventurine-bracelet",
+        paymentLink: "https://buy.stripe.com/test_5kA4hOgmMdyE4jmeUW"
     },
     {
         name: "Green Aventurine and White Crystal Bracelet",
         pic: "./Crystals/3.jpg",
-        price: 799,
-        slug: "green-aventurine-white-crystal"
+        price: 999,
+        slug: "green-aventurine-white-crystal",
+        paymentLink: "https://buy.stripe.com/test_5kA4hOgmMdyE4jmeUW"
     },
     {
         name: "Yellow Citrine Crystal Bracelet",
         pic: "./Crystals/4.jpg",
-        price: 799,
-        slug: "yellow-citrine-crystal"
+        price: 999,
+        slug: "yellow-citrine-crystal",
+        paymentLink: "https://buy.stripe.com/test_5kA4hOgmMdyE4jmeUW"
     },
     {
         name: "Money Magnet Crystal Bracelet",
         pic: "./Crystals/5.webp",
         price: 999,
-        slug: "money-magnet-crystal"
+        slug: "money-magnet-crystal",
+        paymentLink: "https://buy.stripe.com/test_5kA4hOgmMdyE4jmeUW"
     },
     {
         name: "Blue Magnesite Bracelet",
         pic: "./Crystals/6.webp",
-        price: 799,
-        slug: "blue-magnesite-bracelet"
+        price: 999,
+        slug: "blue-magnesite-bracelet",
+        paymentLink: "https://buy.stripe.com/test_5kA4hOgmMdyE4jmeUW"
     },
     {
         name: "Malachite Crystal Bracelet",
         pic: "./Crystals/7.webp",
-        price: 799,
-        slug: "malachite-crystal-bracelet"
+        price: 999,
+        slug: "malachite-crystal-bracelet",
+        paymentLink: "https://buy.stripe.com/test_5kA4hOgmMdyE4jmeUW"
     },
     {
         name: "Rose Quartz Crystal Bracelet",
         pic: "./Crystals/8.webp",
-        price: 799,
-        slug: "rose-quartz-crystal"
+        price: 999,
+        slug: "rose-quartz-crystal",
+        paymentLink: "https://buy.stripe.com/test_5kA4hOgmMdyE4jmeUW"
     },
     {
         name: "Clear Quartz Crystal Bracelet",
         pic: "./Crystals/9.webp",
-        price: 799,
-        slug: "clear-quartz-crystal"
+        price: 999,
+        slug: "clear-quartz-crystal",
+        paymentLink: "https://buy.stripe.com/test_5kA4hOgmMdyE4jmeUW"
     },
     {
         name: "Red Jasper Crystal Bracelet",
         pic: "./Crystals/10.jpg",
-        price: 799,
-        slug: "red-jasper-crystal"
+        price: 999,
+        slug: "red-jasper-crystal",
+        paymentLink: "https://buy.stripe.com/test_5kA4hOgmMdyE4jmeUW"
     },
     {
         name: "Black Lava 7 Chakra Crystal Bracelet",
         pic: "./Crystals/11.webp",
-        price: 899,
-        slug: "black-lava-7-chakra-crystal"
+        price: 999,
+        slug: "black-lava-7-chakra-crystal",
+        paymentLink: "https://buy.stripe.com/test_5kA4hOgmMdyE4jmeUW"
     },
     {
         name: "Black Tourmaline Bracelet",
         pic: "./Crystals/12.webp",
-        price: 899,
-        slug: "black-tourmaline-bracelet"
+        price: 999,
+        slug: "black-tourmaline-bracelet",
+        paymentLink: "https://buy.stripe.com/test_5kA4hOgmMdyE4jmeUW"
     },
     {
         name: "Rudraksha Crystal Bracelet ",
         pic: "./Crystals/13.jpg",
-        price: 899,
-        slug: "rudraksha-crystal-bracelet"
+        price: 999,
+        slug: "rudraksha-crystal-bracelet",
+        paymentLink: "https://buy.stripe.com/test_5kA4hOgmMdyE4jmeUW"
     },
     {
         name: "Amethyst Bracelet",
         pic: "./Crystals/14.jpg",
-        price: 899,
-        slug: "amethyst-bracelet"
+        price: 999,
+        slug: "amethyst-bracelet",
+        paymentLink: "https://buy.stripe.com/test_5kA4hOgmMdyE4jmeUW"
     },
     {
         name: "Bloodstone Bracelet",
         pic: "./Crystals/15.webp",
-        price: 899,
-        slug: "bloodstone-bracelet"
+        price: 999,
+        slug: "bloodstone-bracelet",
+        paymentLink: "https://buy.stripe.com/test_5kA4hOgmMdyE4jmeUW"
     },
     {
         name: "Sodalite Bracelet",
         pic: "./Crystals/16.webp",
-        price: 899,
-        slug: "sodalite-bracelet"
+        price: 999,
+        slug: "sodalite-bracelet",
+        paymentLink: "https://buy.stripe.com/test_5kA4hOgmMdyE4jmeUW"
     },
     {
         name: "Tiger Eye Bracelet",
         pic: "./Crystals/17.jpg",
-        price: 899,
-        slug: "tiger-eye-bracelet"
+        price: 999,
+        slug: "tiger-eye-bracelet",
+        paymentLink: "https://buy.stripe.com/test_5kA4hOgmMdyE4jmeUW"
     },
     {
         name: "Carnelian Bracelet",
         pic: "./Crystals/18.jpg",
-        price: 899,
-        slug: "carnelian-bracelet"
+        price: 999,
+        slug: "carnelian-bracelet",
+        paymentLink: "https://buy.stripe.com/test_5kA4hOgmMdyE4jmeUW"
     },
     {
         name: "Turquoise Bracelet",
         pic: "./Crystals/19.jpg",
-        price: 899,
-        slug: "turquoise-bracelet"
+        price: 999,
+        slug: "turquoise-bracelet",
+        paymentLink: "https://buy.stripe.com/test_5kA4hOgmMdyE4jmeUW"
     },
     {
         name: "Lapis Lazuli Bracelet",
         pic: "./Crystals/20.jpg",
-        price: 899,
-        slug: "lapis-lazuli-bracelet"
+        price: 999,
+        slug: "lapis-lazuli-bracelet",
+        paymentLink: "https://buy.stripe.com/test_5kA4hOgmMdyE4jmeUW"
     },
     {
         name: "Asian Chakra",
         pic: "./Crystals/21.webp",
-        price: 1299,
-        slug: "asian-chakra"
+        price: 999,
+        slug: "asian-chakra",
+        paymentLink: "https://buy.stripe.com/test_5kA4hOgmMdyE4jmeUW"
     },
     {
         name: "Gomati Chakra",
         pic: "./Crystals/22.jpg",
         price: 7500,
-        slug: "gomati-chakra"
+        slug: "gomati-chakra",
+        paymentLink: "https://buy.stripe.com/test_9AQdSoeeE0LSeY0eUX"
     },
     {
         name: "Money Coins",
         pic: "./Crystals/23.webp",
-        price: 799,
-        slug: "money-coins"
+        price: 999,
+        slug: "money-coins",
+        paymentLink: "https://buy.stripe.com/test_5kA4hOgmMdyE4jmeUW"
     },
     {
         name: "Love Coins",
         pic: "./Crystals/24.webp",
-        price: 799,
-        slug: "love-coins"
+        price: 999,
+        slug: "love-coins",
+        paymentLink: "https://buy.stripe.com/test_5kA4hOgmMdyE4jmeUW"
     },
-    
-
     {
         name: "Dakshineswar Kali Temple (E-Puja)",
         pic: "./Dakshineswar.jpg",
